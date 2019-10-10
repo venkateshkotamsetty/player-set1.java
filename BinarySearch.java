0b0c5fa34af01309bcef4e6826fdb390121cdf53
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2019-10-10 07:41:24 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -7,12 +7,12 @@ class Main
     int i, first, last, middle, n, search, array[];
  
     Scanner in = new Scanner(System.in);
-   // System.out.println("Enter number of elements");
+   
     n = in.nextInt();
-    //System.out.println("Enter value to find");
+    
     search = in.nextInt();
     array = new int[n];
-   // System.out.println("Enter " + n + " integers");
+   
     for (i = 0; i < n; i++)
     {
       array[i] = in.nextInt();
